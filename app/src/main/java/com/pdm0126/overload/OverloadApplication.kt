ffeb57f0e4f9cdc2818f165d116dc31c56b341d0
IgnacioPeralta00 <00196321@uca.edu.sv>
2026-06-09 01:21:20 -0600

    feat: configurar OverloadApplication e inyección manual de dependencias

---
+++ app/src/main/java/com/pdm0126/overload/OverloadApplication.kt
@@ -0,0 +1,18 @@
+package com.pdm0126.overload
+
+import android.app.Application
+import com.pdm0126.overload.data.local.OverloadDatabase
+import com.pdm0126.overload.data.repository.ExerciseRepositoryImp
+import com.pdm0126.overload.domain.repository.ExerciseRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class OverloadApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { OverloadDatabase.getDatabase(this, applicationScope) }
+
+    val exerciseRepository: ExerciseRepository by lazy {
+        ExerciseRepositoryImp(database.exerciseDao())
+    }
+}
+
