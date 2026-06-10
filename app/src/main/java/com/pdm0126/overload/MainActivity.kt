2ad1e3a0aa34938f1ba2b09f073335b9b8e7cd6d
IgnacioPeralta00 <00196321@uca.edu.sv>
2026-06-09 20:07:14 -0600

    test: implementado con exito en MainActivity

--- app/src/main/java/com/pdm0126/overload/MainActivity.kt
+++ app/src/main/java/com/pdm0126/overload/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pdm0126.overload.ui.screens.TestScreen
 import com.pdm0126.overload.ui.theme.OverLoadTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OverLoadTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TestScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OverLoadTheme {
-        Greeting("Android")
     }
 }
